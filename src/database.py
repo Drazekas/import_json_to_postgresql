22eabf7b43ad5fd9662c5bdbd8bb825926cb5344
Drazekas <101950824+Drazekas@users.noreply.github.com>
2022-05-26 01:01:23 +0200

    Add docstrings to packages

--- src/database.py
+++ src/database.py
@@ -6,16 +6,63 @@ import typing
 
 
 class Database:
+    """
+    A class used to connect to Postgres database
+
+    ...
+
+    Attributes
+    ----------
+    db_params : str
+        database parameters to establish a connection
+    conn : connection
+        database connection
+    cur : cursor
+        database cursor
+
+    Methods
+    -------
+    create_table(sql)
+        Creates database table
+    insert_values(sql, data)
+        Inserts data from python script to database table
+    insert_by_select_from(sql)
+        Inserts data from database table to database table
+    select_all(sql, values=None)
+        Selects data from database
+    """
+
     def __init__(self, **db_params:str) -> None:
+        """
+        Parameters
+        ----------
+        db_params : str
+            database parameters to establish a connection
+        conn : connection
+            database connection
+        cur : cursor
+            database cursor
+        """
+
         self.conn = psycopg2.connect(**db_params)
         self.cur = self.conn.cursor()
 
     def __del__(self) -> None:
+        """Rollbacks and cleans connection when references to the object have been deleted"""
+
         self.conn.rollback()
         self.cur.close()
         self.conn.close()
 
     def create_table(self, sql:str) -> None:
+        """Executes SQL statement for creating or dropping table
+
+        Parameters
+        ----------
+        sql: str
+            SQL statement for creating or dropping table
+        """
+
         try:
             self.cur.execute(sql)
             self.conn.commit()
@@ -25,6 +72,16 @@ class Database:
             self.conn.rollback()
 
     def insert_values(self, sql:str, data:list[tuple]) -> None:
+        """Inserts data from python script to database table
+
+        Parameters
+        ----------
+        sql: str
+            SQL 'INSERT INTO ... VALUES ...' statement
+        data: list[tuple]
+            List of tuples of values to insert into table 
+        """
+
         try:
             execute_values(self.cur, sql, data)
             self.conn.commit()
@@ -32,6 +89,14 @@ class Database:
             self.conn.rollback()
     
     def insert_by_select_from(self, sql:str) -> None:
+        """Inserts data from database table to database table
+        
+        Parameters
+        ----------
+        sql: str
+            SQL 'INSERT INTO ... SELECT * FROM ...' statement
+        """
+
         try:
             self.cur.execute(sql)
             self.conn.commit()
@@ -41,6 +106,21 @@ class Database:
     def select_all(self, sql:str, 
                 values:typing.Dict[str,typing.Any]=None
                 ) -> tuple[list[str], list[tuple]]:
+        """Selects data from database
+
+        Parameters
+        ----------
+        sql: str
+            SQL 'SELECT * FROM ...' statement with query placeholders (%s)
+        values: Dict[str, Any], optional
+            Values inserted into query placeholders
+            
+        Returns
+        -------
+        tuple[list[str], list[tuple]]
+            tuple[list[column names], list[tuple[query records]]]
+        """
+
         self.cur.execute(sql, values)
         column_names:list[str] = [desc[0] for desc in self.cur.description]
         data:list[tuple] = self.cur.fetchall()
@@ -48,6 +128,21 @@ class Database:
 
 
 def db_config(filename:str, section:str) -> typing.Dict[str,str]:
+    """Loads database parameters from .ini file
+
+    Parameters
+    ----------
+    filename: str
+        The filename of .ini file (if .ini is located in main folder) or path (otherwise)
+    section:str
+        Section name of the .ini file
+        
+    Returns
+    -------
+    dict
+        Dictionary with database parameters
+    """
+
     parser = ConfigParser()
     parser.read(filename)
     db:typing.Dict[str,str] = {}
--- src/logging_tools.py
+++ src/logging_tools.py
@@ -1,3 +1,12 @@
+"""Logging tools
+
+This module contains a decorator for logging the flow of running functions in your script.
+
+This module contains the following decorator:
+
+    * func_status - wrap you function with logging to file
+"""
+
 import logging
 from time import perf_counter
 
@@ -10,6 +19,20 @@ logger = logging.getLogger('logger')
 
 
 def func_status(func):
+    """Gets function and wraps it with logging to file
+    Logs start of the function and end of the function with a running time 
+
+    Parameters
+    ----------
+    func : function
+        The function to be wraped
+
+    Returns
+    -------
+    function
+        Wrapped function
+    """
+
     def wrapper(*args, **kwargs):
         logger.info(f'Entered function: {func.__name__}')
         time_start = perf_counter()
